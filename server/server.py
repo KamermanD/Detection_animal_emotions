06a4f6a5c22535d58f99a901ae3a1441fba7a1df
DKamerman <rubin5341@yandex.ru>
2024-12-27 17:35:54 +0300

    Added DataLoadRequest and modify endpoint Load_dataset

--- server/server.py
+++ server/server.py
@@ -13,21 +13,24 @@ process_status = {}
 
 class DatasetLoadResponse(BaseModel):
     message: str
+    
+class DataLoadRequest(BaseModel):
+    folder: UploadFile = File(...)
 
 @app.post("/load_dataset", response_model=DatasetLoadResponse, tags=["load_dataset"])
-async def load_dataset(folder: UploadFile = File(...)):
+async def load_dataset(requests: DataLoadRequest):
     
-    process_status["load_dataset"] = folder.filename
+    process_status["load_dataset"] = requests.filename
     
     dir_datasets = [dir for dir in os.listdir("datasets") if os.path.isdir(os.path.join("datasets", dir))]
-    if folder.filename in dir_datasets:
+    if requests.filename in dir_datasets:
         raise HTTPException(status_code=400, detail="Датасет с таким именем уже есть")
-    if not folder.filename.endswith(".zip"):
+    if not requests.filename.endswith(".zip"):
         raise HTTPException(status_code=400, detail="Только ZIP архив необходимо загружать")
     
-    zip_path = os.path.join("datasets", folder.filename)
+    zip_path = os.path.join("datasets", requests.filename)
     with open(zip_path, "wb") as temp:
-        temp.write(await folder.read())
+        temp.write(await requests.read())
         
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall("datasets")
@@ -36,7 +39,7 @@ async def load_dataset(folder: UploadFile = File(...)):
     
     process_status["load_dataset"] = ""
     
-    return DatasetLoadResponse(message=f"Dataset {folder.filename} загружен!")
+    return DatasetLoadResponse(message=f"Dataset {requests.filename} загружен!")
 
 if __name__ == "__main__":
     uvicorn.run("server:app", host="127.0.0.1", port=8000, reload=True)
