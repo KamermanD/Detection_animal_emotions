abc09727b3c3abc829a7a2dfc824ac0c6f9acafb
DKamerman <rubin5341@yandex.ru>
2024-12-28 22:53:22 +0300

    Modify endpoints fit and load_model.

--- server/server.py
+++ server/server.py
@@ -3,11 +3,24 @@ from fastapi import FastAPI, UploadFile, File, HTTPException
 from pydantic import BaseModel
 import os
 import zipfile
+import numpy as np
+import pandas as pd
+from torch.utils.data import DataLoader
 from typing import List
+import joblib
+from joblib import load
+import torch
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV
+from torchvision.models import resnet18, ResNet18_Weights
+from torchvision.transforms import v2
 from models.request_models import FitRequest, ModelLoadRequest, ModelRemoveRequest, DatasetRemoveRequest
 from models.response_models import DatasetLoadResponse, FitResponse, ModelLoadResponse 
 from models.response_models import ModelsListResponse, DatasetsListResponse, ModelRemoveResponse
 from models.response_models import DatasetRemoveResponse, AllModelsRemoveResponse, AllDatasetsRemoveResponse
+from trainer_core.upload_dataset import upload_emotion_class, upload_dataset_inframe
+from trainer_core.dataset import Dataset
+from trainer_core.extraction import Extraction
 
 app = FastAPI(
     docs_url="/api/openapi",
@@ -15,6 +28,7 @@ app = FastAPI(
 )
 
 process_status = {}
+models = {}
 
 @app.post("/load_dataset", response_model=DatasetLoadResponse, tags=["upload_file"])
 async def load_dataset(file: UploadFile = File(...)):
@@ -47,11 +61,85 @@ async def load_dataset(file: UploadFile = File(...)):
 
 @app.post("/fit", response_model=FitResponse, tags=["trainer"])
 async def fit(requests: FitRequest):
-    return FitResponse(message=f"Модель '{requests.config.id_model}' обучена и сохранена.") 
+    
+    dataset = requests.name_dataset
+    os.makedirs("datasets", exist_ok=True)
+    datasets_list = os.listdir("datasets")
+    if dataset not in datasets_list:
+            raise HTTPException(status_code=400, detail=f"Датасета '{dataset}' нет на сервере")
+    
+    list_emotion = upload_emotion_class(dataset)
+    model_id = requests.config.id_model
+    
+    os.makedirs("models_train", exist_ok=True)
+    models_list = [os.path.splitext(file)[0] for file in os.listdir("models_train")]
+    
+    if model_id in models_list:
+        raise HTTPException(status_code=400, detail=f"Модель '{model_id}' уже существует")
+    
+    if list_emotion['emotions_count'] < 2:
+        raise HTTPException(status_code=400, detail="Необходимо загрузить датасет с двумя классами или более")
+    
+    if not list_emotion['emotions_list']:
+        raise HTTPException(status_code=400, detail="В директории с датасетом должны быть вложенные директории, название которых соответсвуют названию класса")
+    
+    emotions = list_emotion['emotions_list']    
+    df = upload_dataset_inframe(dataset, emotions)
+    
+    if df.empty:
+        raise HTTPException(status_code=400, detail="В датасете нет изображений")
+    
+    emotion_labels_map = {}
+
+    unique_emotions = df['emotion'].unique()
+    for i in range(len(unique_emotions)):
+        emotion_labels_map[unique_emotions[i]] = i
+    df['emotion_label'] = df['emotion'].apply(lambda x: emotion_labels_map[x])
+    
+    y_train = df["emotion_label"]
+    X_train = df["img"]
+    X_train.index = np.arange(len(X_train))
+    y_train.index = np.arange(len(y_train))
+    
+    transform_test = v2.Compose([
+    v2.Resize(size=(224, 224)),
+    v2.PILToTensor(),
+    v2.ToDtype(torch.float32, scale=True),
+    v2.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    
+    train_ds = Dataset(pd.concat([X_train, y_train], axis=1), transform_test)
+    train_loader = DataLoader(train_ds, batch_size=32, num_workers=4)
+    
+    model = resnet18(weights=ResNet18_Weights.DEFAULT)
+    feature_extractor = torch.nn.Sequential(*list(model.children())[:-1])
+    
+    ext = Extraction(feature_extractor)
+    train_feature, train_label = ext.extract(train_loader)
+    
+    param = {
+    "kernel": requests.config.hyperparameters.kernal,
+    'C': requests.config.hyperparameters.C
+    }
+    
+    svc = svm.SVC()
+    svm_grid = GridSearchCV(svc, param_grid=param, verbose=2, n_jobs=-1)
+
+    svm_grid.fit(train_feature, train_label)
+    
+    model_path = f"models_train/{model_id}.joblib"
+    joblib.dump(svm_grid, model_path)
+    
+    return FitResponse(message=f"Модель '{requests.config.id_model}' обучена и сохранена.")
+
 
 
 @app.post("/load_model", response_model=ModelLoadResponse, tags=["upload_file"])
 async def load_model(requests: ModelLoadRequest):
+    global models
+    model_id = requests.id
+    model_path = f"models_train/{model_id}.joblib"
+    models[model_id] = load(model_path)
     return ModelLoadResponse(message=f"Модель '{requests.id_model}' загружена.")
 
 @app.get("/list_models", response_model=ModelsListResponse, tags=["upload_file"])
